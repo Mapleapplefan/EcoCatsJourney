4a62fe1da0712ad1941ae98ba5202094e2700ea9
Mapleapplefan <mapleapplefan@gmail.com>
2024-06-26 17:55:46 -0700

    Placeholders

---
+++ EcoCatJourney/Assets/Resources/Scripts/AnimationController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//You don't need to worry about this script,
+//it deals with all the animations taken from the player controller
+[RequireComponent(typeof(Animator))]
+public class AnimationController : MonoBehaviour
+{
+    private PlayerController m_playerController;
+    private Animator m_animator;
+    void Start()
+    {
+        m_playerController = GetComponent<PlayerController>();
+        m_animator = GetComponent<Animator>();
+        m_animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("Animations/_MarioAnimator");
+        transform.gameObject.layer = 3;
+        m_playerController.GroundLayer = LayerMask.GetMask("Default");
+        transform.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+    void Update()
+    {
+        //Plays jump animation
+        if (!m_playerController.m_grounded)
+        {
+            m_animator.SetBool("Jump", true);
+        }
+        else
+        {
+            m_animator.SetBool("Jump", false);
+        }
+
+        //Plays walking animation
+        if (m_playerController.m_horizontalMovement.x != 0)
+        {
+            m_animator.SetBool("Run", true);
+        }
+        else
+        {
+            m_animator.SetBool("Run", false);
+        }
+
+        //Flips the player in the right direction
+        if (m_playerController.m_horizontalMovement.x > 0)
+        {
+            m_playerController.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (m_playerController.m_horizontalMovement.x < 0)
+        {
+            m_playerController.transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
---
+++ EcoCatJourney/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    [Header("Player Settings")]
+    public int Speed = 5;
+    public float JumpForce = 100;
+
+    [Header("Ground Checking")]
+    public LayerMask GroundLayer = 0;
+
+    private Rigidbody2D m_rigidbody;
+    [HideInInspector] public Vector2 m_horizontalMovement;
+    [HideInInspector] public bool m_grounded;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        m_horizontalMovement.x = Input.GetAxis("Horizontal");
+        m_rigidbody.velocity = new Vector2(m_horizontalMovement.x * Speed, m_rigidbody.velocity.y);
+        m_grounded = Physics2D.OverlapCircle(transform.position + new Vector3(0, -0.5f, 0), .1f, GroundLayer);
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (m_grounded && Input.GetKeyDown(KeyCode.Space)) {
+            m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x, JumpForce*3);
+        }
+    }
+}
