354b9aeac10292f896478edcba24d5ac1de62ce2
Mapleapplefan <mapleapplefan@gmail.com>
2024-07-02 13:17:35 -0700

    Fixed Jumping Issue

--- EcoCatJourney/Assets/Resources/Scripts/PlayerController.cs
+++ EcoCatJourney/Assets/Resources/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController: MonoBehaviour
 {
     [Header("Player Settings")]
     public int Speed = 5;
-    public float JumpForce = 1;
+    public float JumpForce = 3;
 
     [Header("Ground Checking")]
     public LayerMask GroundLayer = 0;
@@ -16,21 +16,23 @@ public class PlayerController: MonoBehaviour
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
+
     }
 
 
     void Update()
     {
-        m_horizontalMovement.x = Input.GetAxis("Horizontal");
-        m_rigidbody.velocity = new Vector2(m_horizontalMovement.x * Speed, m_rigidbody.velocity.y);
-        m_grounded = Physics2D.OverlapCircle(transform.position + new Vector3(0, -0.5f, 0), .1f, GroundLayer);
-        Jump();
+       m_horizontalMovement.x = Input.GetAxis("Horizontal");
+       m_rigidbody.velocity = new Vector2(m_horizontalMovement.x * Speed, m_rigidbody.velocity.y);
+       m_grounded = Physics2D.OverlapCircle(transform.position + new Vector3(0, -0.5f, 0), .1f, GroundLayer);
+       Jump();
+
     }
 
     void Jump()
     {
-        if (m_grounded && Input.GetKeyDown(KeyCode.Space)) {
-            m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x, JumpForce);
+        if (Input.GetKeyDown(KeyCode.Space) && m_grounded) {
+            m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x, JumpForce*2);
         }
     }
 }
